928aa76363b931303ef4f676102d2a35a8a897fc
Ubuntu <vinod.thakur@mindtree.com>
2020-06-12 02:01:39 +0000

    I am pushing all the files to my GitHub

---
+++ prj2.java
@@ -0,0 +1,5 @@
+class HelloJava2 {
+Public static void main(String args[])[
+System.out.PrintIn("I am your Java Program for Project 2. Thanks you!");
+	}
+}
---
+++ prj2.js
@@ -0,0 +1,2 @@
+var texts = "II am your JavaScript Program for Project2";
+console.log(texts);
